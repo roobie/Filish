9e2c7d3330313bbff500277b70224dfd42c7d53a
Bjorn Roberg <bjorn.roberg@bool.se>
2017-06-02 21:38:00 +0200

    Initial commit, with an almost usable application. Kinda bloaty, though, so that's something to work with

---
+++ main.pas
@@ -0,0 +1,101 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  FileUtil,
+  Forms,
+  Controls,
+  Graphics,
+  Dialogs,
+  StdCtrls,
+  ComCtrls,
+  HlpHashFactory,
+  HlpIHash,
+  HlpIHashResult;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    Memo1: TMemo;
+    StatusBarMain: TStatusBar;
+    procedure FormActivate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormActivate(Sender: TObject);
+var
+  Mode: string;
+  Fnamesrc: string;
+  FSHA2_512: IHash;
+  Result: IHashResult;
+  ResultStr: string;
+  Fnamesig: string;
+begin
+  MainForm.Caption := 'Filish v1.0.0';
+  Mode := ParamStr(1);
+  Fnamesrc := ParamStr(2);
+  Fnamesig := Concat(Fnamesrc, '.sha512.sig.txt');
+  FSHA2_512 := THashFactory.TCrypto.CreateSHA2_512();
+  Result := FSHA2_512.ComputeFile(Fnamesrc);
+  ResultStr := Result.ToString();
+
+  if Mode = 'generate' then
+  begin
+
+    MainForm.Memo1.Text := ResultStr;
+
+    with TStringList.Create do
+      try
+        Add(ResultStr);
+        SaveToFile(Fnamesig);
+      finally
+        Free;
+      end;
+
+    MainForm.StatusBarMain.Panels[0].Text :=
+      Concat('OK, sig written to: ', Fnamesig);
+  end
+  else if Mode = 'check' then
+  begin
+    with TStringList.Create do
+      try
+        LoadFromFile(Fnamesig);
+        Add(ResultStr);
+        MainForm.Memo1.Text:=GetText;
+        if CompareText(ResultStr, GetText) = 0 then
+        begin
+          MainForm.StatusBarMain.Panels[0].Text:='OK';
+          //MainForm.Memo1.Text:='OK, hash of file matched contents of sig file';
+        end
+        else
+        begin
+          MainForm.StatusBarMain.Panels[0].Text:='NOT OK';
+          //MainForm.Memo1.Text:='Warning! Hash of file does not match contents of sig file';
+        end;
+      finally
+        Free;
+      end;
+  end;
+end;
+
+end.
+
